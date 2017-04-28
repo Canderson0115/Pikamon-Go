b0f61de8b0c5ed5e5b0f5d0cf394cd280b17e935
student5 <student0@jh-mba158-10.d214.org>
2017-04-28 14:18:37 -0500

    Pikamen Class

---
+++ Pikamon Go!/PikamenClass.swift
@@ -0,0 +1,20 @@
+//
+//  PikamenClass.swift
+//  Pikamon Go!
+//
+//  Created by student0 on 4/28/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+class Pikamen
+{
+    var name = String()
+    var type = String()
+    var health = Int()
+    var alive = Bool()
+    var moveSet = [String]()
+    var effects = [String]()
+    
+}
