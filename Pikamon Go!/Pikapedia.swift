0502a0d679de897b10875f0ddd8e5098d56f6e24
student5 <student8@jh-mba158-23.d214.org>
2017-04-28 14:52:54 -0500

    updated battle view and pikapedia

---
+++ Pikamon Go!/Pikapedia.swift
@@ -0,0 +1,25 @@
+//
+//  Pikapedia.swift
+//  Pikamon Go!
+//
+//  Created by student8 on 4/28/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class Pikapedia: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+
+    
+
+}
