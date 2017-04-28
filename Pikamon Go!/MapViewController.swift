c33f9b981d8457a766bcdb4e4698d8baaf3f6bb9
student5 <student0@jh-mba158-10.d214.org>
2017-04-28 14:56:07 -0500

    mastiff

--- Pikamon Go!/MapViewController.swift
+++ Pikamon Go!/MapViewController.swift
@@ -12,13 +12,34 @@ import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
 {
+    @IBOutlet weak var mapViewBoard: MKMapView!
+    
     //42.108516, -88.001374 : 42.073481, -87.937314
     
     func createLocation() -> CLLocationCoordinate2D
     {
         
-        let location = CLLocationCoordinate2D()
+        var x = CGFloat(42073481)
+        var y = CGFloat(87937314)
+        let rand_x = CGFloat(arc4random_uniform(35035))
+        let rand_y = CGFloat(arc4random_uniform(64060))
+        x = x + rand_x
+        y = y + rand_y
+        x = x / 100000
+        let xCoordinate = CLLocationDegrees(x)
+        let yCoordinate = CLLocationDegrees(y)
+        
+        let location = CLLocationCoordinate2D(latitude: xCoordinate, longitude: yCoordinate)
+        
         
         return location
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let pin = MKPointAnnotation()
+        pin.coordinate = createLocation()
+        
+        mapViewBoard.addAnnotation(pin)
     }
 }
