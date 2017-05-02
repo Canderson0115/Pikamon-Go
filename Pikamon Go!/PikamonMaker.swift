3d3f1368f1f2a788e584af12082f3efe93b62de6
student5 <student8@jh-mba158-23.d214.org>
2017-05-02 14:31:19 -0500

    new update

---
+++ Pikamon Go!/PikamonMaker.swift
@@ -0,0 +1,52 @@
+//
+//  PikamonMaker.swift
+//  Pikamon Go!
+//
+//  Created by student8 on 5/2/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+//Parameters for Pikamon
+
+class Parameters
+{
+    var name = String()
+    var type = String()
+    var health = Int()
+    var alive = Bool()
+    var moveSet = [String]()
+    var effects = [String]()
+    var weight = Double()
+    
+    init(name: String, type: String, health: Int, alive: Bool, moveSet: [String], effects: [String], weight: Double)
+    {
+        self.name = name
+        self.type = type
+        self.health = health
+        self.alive = alive
+        self.moveSet = moveSet
+        self.effects = effects
+        self.weight = weight
+        
+    }
+    
+
+    
+}
+
+class Pikamon
+{
+    
+    var LizieChu = Parameters(name: "LizieChu", type: "Grass-Electro", health: 250, alive: true, moveSet: ["Slash","Leaf Blast","Summon Spark","Flower Bloom"], effects: [""], weight: 0.2)
+    
+    var Glacierite = Parameters(name: "Glacierite", type: "Ice-Rock", health: 250, alive: true, moveSet: ["ExampleMove"], effects: ["ExampleEffect"], weight: 0.2)
+    
+    var Bochtite = Parameters(name: "Bochtite", type: "Ice-Rock", health: 250, alive: true, moveSet: ["ExampleMove"], effects: ["ExampleEffect"], weight: 0.2)
+    
+    var Trithyta = Parameters(name: "Trithyta", type: "Matter-Electro", health: 500, alive: true, moveSet: ["ExampleMove"], effects: ["ExampleEffect"], weight: 0.2)
+    
+    var pokéchew = Parameters(name: "Pokéchew", type: "Electro", health: 100, alive: true, moveSet: ["ExampleLightningMove"], effects: [""], weight: 0.2)
+    
+}
--- Pikapedia.swift
+++ Pikapedia.swift
@@ -2,7 +2,7 @@
 //  Pikapedia.swift
 //  Pikamon Go!
 //
-//  Created by student8 on 4/28/17.
+//  Created by student8 on 5/2/17.
 //  Copyright © 2017 John Hersey High School. All rights reserved.
 //
 
@@ -10,16 +10,18 @@ import UIKit
 
 class Pikapedia: UIViewController {
 
+@IBOutlet weak var PikamonList: UITableView!
     
+var PikamonViewer:[Pikamon] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
         
     }
 
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return PikamonViewer.count
+    }
 
 }
