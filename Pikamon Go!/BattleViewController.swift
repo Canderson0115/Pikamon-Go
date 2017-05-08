91af314e7416a5c51b92e8559eaacf443df50ce0
student5 <student8@jh-mba158-23.local>
2017-05-08 14:54:08 -0500

    battle basics

--- Pikamon Go!/BattleViewController.swift
+++ Pikamon Go!/BattleViewController.swift
@@ -18,34 +18,48 @@ class BattleViewController: UIViewController {
     @IBOutlet weak var teamHealthLabel: UILabel!
     @IBOutlet weak var teamImageView: UIImageView!
     
-    @IBOutlet weak var skillOneLabel: UILabel!
-    @IBOutlet weak var skillTwoLabel: UILabel!
-    @IBOutlet weak var skillThreeLabel: UILabel!
-    @IBOutlet weak var skillFourLabel: UILabel!
+    @IBOutlet weak var skill1Button: UIButton!
+    @IBOutlet weak var skill2Button: UIButton!
+    @IBOutlet weak var skill3Button: UIButton!
+    @IBOutlet weak var skill4Button: UIButton!
     
-    var pikamonEnemies = PikamonClass()
-    //    var player = currentPikamon()
+    @IBOutlet weak var gameConsole: UILabel!
+    
+    var pikamonEnemies = PikamonEnemies()
+    var pikamonEnemiesClass2 = PikamonEnemies()
+    
+    var player = Player()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blue
         
-        //        teamNameLabel.text = "\(player.currentPikamonName)"
-        //        teamHealthLabel.text = "\(player.currentPikamon.health)H / \(player.currentPikamon.health)H"
+        teamNameLabel.text = "\(player.pikamonInInventory[0].name)"
+        teamHealthLabel.text = "\(player.pikamonInInventory[0].health)H / \(player.pikamonInInventory[0].health)H"
         
+        skill1Button.setTitle("\(player.pikamonInInventory[0].moveSet[0].name)", for: .normal)
+  //      skill2Button.setTitle("\(player.pikamonInInventory[0].moveSet[1].name)", for: .normal)
+  //      skill3Button.setTitle("\(player.pikamonInInventory[0].moveSet[2].name)", for: .normal)
+  //      skill4Button.setTitle("\(player.pikamonInInventory[0].moveSet[3].name)", for: .normal)
         
-        let randomPikamon = 1 + Int(arc4random_uniform(5))
+        let randomPikamon = 1 + Int(arc4random_uniform(1))
         
         if randomPikamon == 1
         {
             enemyNameLabel.text = "\(pikamonEnemies.LizieChu.name)"
             enemyHealthLabel.text = "\(pikamonEnemies.LizieChu.health)H / \(pikamonEnemies.LizieChu.health)H"
             enemyImageView.image = UIImage(named: "lizzichuImage")!
+            gameConsole.text = "[Matt] Lizzichu I release you!"
+            
             
         } else if randomPikamon == 2
         {
             enemyNameLabel.text = "\(pikamonEnemies.Glacierite.name)"
             enemyHealthLabel.text = "\(pikamonEnemies.Glacierite.health)H / \(pikamonEnemies.Glacierite.health)H"
+            
+            gameConsole.text = "[Matt] Glacierite I release you!"
             
             
         } else if randomPikamon == 3
@@ -53,11 +67,15 @@ class BattleViewController: UIViewController {
             enemyNameLabel.text = "\(pikamonEnemies.Bochtite.name)"
             enemyHealthLabel.text = "\(pikamonEnemies.Bochtite.health)H / \(pikamonEnemies.Bochtite.health)H"
             
+            gameConsole.text = "[Matt] Bochtite I release you!"
+            
             
         } else if randomPikamon == 4
         {
             enemyNameLabel.text = "\(pikamonEnemies.Trithyta.name)"
             enemyHealthLabel.text = "\(pikamonEnemies.Trithyta.health)H / \(pikamonEnemies.Trithyta.health)H"
+            
+            gameConsole.text = "[Matt] Trithyta I release you!"
             
             
         } else if randomPikamon == 5
@@ -65,9 +83,37 @@ class BattleViewController: UIViewController {
             enemyNameLabel.text = "\(pikamonEnemies.Pokéchew.name)"
             enemyHealthLabel.text = "\(pikamonEnemies.Pokéchew.health)H / \(pikamonEnemies.Pokéchew.health)H"
             enemyImageView.image = UIImage(named: "pokechewimage")!
+            gameConsole.text = "[Matt] Pokéchew I release you!"
             
         }
         
     }
 
+    @IBAction func skill1Tapped(_ sender: UIButton)
+    {
+        let blockChance = 1 + Int(arc4random_uniform(10))
+        
+        if blockChance <= 2
+        {
+            gameConsole.text = "Enemy Pikamon blocked incoming damage"
+        }
+        else
+        {
+            if player.pikamonInInventory[0].moveSet[0].name == "punch"
+            {
+                pikamonEnemies.LizieChu.health -= player.pikamonInInventory[0].moveSet[0].damage
+/*              pikamonEnemies.Bochtite.health -= player.pikamonInInventory[0].moveSet[0].damage
+                pikamonEnemies.Firemander.health -= player.pikamonInInventory[0].moveSet[0].damage
+                pikamonEnemies.Glacierite.health -= player.pikamonInInventory[0].moveSet[0].damage
+                pikamonEnemies.Lonelysore.health -= player.pikamonInInventory[0].moveSet[0].damage
+                pikamonEnemies.Pokéchew.health -= player.pikamonInInventory[0].moveSet[0].damage
+                pikamonEnemies.Trithyta.health -= player.pikamonInInventory[0].moveSet[0].damage
+ */
+                
+                enemyHealthLabel.text = "\(pikamonEnemies.LizieChu.health)H / \(pikamonEnemiesClass2.LizieChu.health)H"
+                gameConsole.text = " "
+            }
+        }
+    }
+    
 }
---
+++ Pikamon Go!/PikamonStruct.swift
@@ -0,0 +1,117 @@
+//
+//  PikamonStruct.swift
+//  Pikamon Go!
+//
+//  Created by student8 on 5/8/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+struct PikamonEnemies
+{
+    
+        
+        var LizieChu = Parameters(name: "LizieChu", type: [types.normal], health: 250, alive: true, moveSet: [moves.punch], effects: [""], weight: 0.2)
+        
+        var Glacierite = Parameters(name: "Glacierite", type: [types.normal], health: 250, alive: true, moveSet: [moves.punch], effects: ["ExampleEffect"], weight: 0.2)
+        
+        var Bochtite = Parameters(name: "Bochtite", type: [types.normal], health: 250, alive: true, moveSet: [moves.punch], effects: ["ExampleEffect"], weight: 0.2)
+        
+        var Trithyta = Parameters(name: "Trithyta", type: [types.normal], health: 500, alive: true, moveSet: [moves.punch], effects: ["ExampleEffect"], weight: 0.2)
+        
+        var Pokéchew = Parameters(name: "Pokéchew", type: [types.normal], health: 100, alive: true, moveSet: [moves.punch], effects: [""], weight: 0.2)
+        
+        var Firemander = Parameters(name: "Firemander", type: [types.fire], health: 25, alive: true, moveSet: [moves.punch, moves.flameTosser], effects: [""], weight: 0.01)
+        
+        var Lonelysore = Parameters(name: "Lonelysore", type: [types.normal], health: 30, alive: true, moveSet: [moves.punch], effects: [""], weight: 0.01)
+        
+        var pikamonList = [Parameters]()
+        
+        init()
+        {
+            pikamonList = [Pokéchew, LizieChu, Glacierite, Bochtite, Trithyta, Firemander]
+        }
+        
+        //var LizieChu = Parameters(name: "LizieChu", type: "Grass-Electro", health: 250, alive: true, moveSet: ["Slash","Leaf Blast","Summon Spark","Flower Bloom"], effects: [""], weight: 0.2)
+        
+        //var Glacierite = Parameters(name: "Glacierite", type: "Ice-Rock", health: 250, alive: true, moveSet: ["ExampleMove"], effects: ["ExampleEffect"], weight: 0.2)
+        
+        //var Bochtite = Parameters(name: "Bochtite", type: "Ice-Rock", health: 250, alive: true, moveSet: ["ExampleMove"], effects: ["ExampleEffect"], weight: 0.2)
+        
+        //var Trithyta = Parameters(name: "Trithyta", type: "Matter-Electro", health: 500, alive: true, moveSet: ["ExampleMove"], effects: ["ExampleEffect"], weight: 0.2)
+        
+        //var Pokéchew = Parameters(name: "Pokéchew", type: "Electro", health: 100, alive: true, moveSet: ["ExampleLightningMove"], effects: [""], weight: 0.2)
+        
+        /*func makeIterator() -> PikamonIterator
+         {
+         return PikamonIterator(self)
+         }
+         */
+        
+        /*func generate() -> AnyIterator<Any>{
+         let nextIndex = pikamonList.count-1 // Construct a AnyGenerator instance, passing a closure that returns the next car in the iteration
+         return AnyIterator { if (nextIndex < 0) { return nil }
+         return self.pikamonList[nextIndex-1] } }
+         
+         func listPropertiesWithValues(reflect: Mirror? = nil) -> [String]
+         {
+         var returnArray  = [String]()
+         let mirror = reflect ?? Mirror(reflecting: self)
+         for (index, attr) in mirror.children.enumerated() {
+         if let property_name = attr.label as String!
+         {
+         
+         returnArray.append(" \(index):  \(property_name) ")
+         }
+         }
+         return returnArray
+         }
+         
+         func dfs()
+         {
+         for i in pikamon
+         }
+         
+         */
+        
+        
+    }
+    
+    
+    /*struct PikamonIterator:IteratorProtocol
+     {
+     var pikamon:PikamonClass  // you must know the group in question
+     var pointer:Parameters?  // you must know where you are
+     var i = 0
+     
+     init(_ pikamon:PikamonClass)
+     {
+     // so note those two items
+     self.pikamon = pikamon
+     self.pointer = pikamon.example1
+     }
+     
+     mutating func next() -> Parameters?
+     {
+     // return the next one; careful to return nil at end
+     if self.i != 5
+     {
+     print("oop")
+     let thisOne = self.pointer
+     self.pointer = self.next()
+     self.i = self.i + 1
+     return thisOne
+     }
+     else
+     {
+     return nil
+     }
+     
+     }
+     }
+     
+     */
+    
+    var pikamonEnemies = PikamonEnemies()
+
