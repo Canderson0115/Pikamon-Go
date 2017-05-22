ff64efdb5d0ef358e63ae000859d6e9027274ff2
student5 <student8@jh-mba158-02.d214.org>
2017-05-22 14:11:44 -0500

    w

---
+++ Pikamon Go!/InventoryViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PikamonInInventoryViewController.swift
+//  Pikamon Go!
+//
+//  Created by student8 on 5/22/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    
+
+}
